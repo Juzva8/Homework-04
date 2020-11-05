a462a1d513726d713fc0ddf85d9e40a1b49616b6
Juzva8 <72834046+Juzva8@users.noreply.github.com>
2020-11-04 18:17:54 -0800

    added realy cool theme song

--- Score.js
+++ Score.js
@@ -2,15 +2,17 @@ var storedScores = JSON.parse(localStorage.getItem("userData"));
 var highScores = document.querySelector("#highScoresList");
 var again = document.querySelector("#again");
 var clearBtn = document.querySelector("#clearScores");
-var Startbtn = document.querySelector("#start-btn")
+var Startbtn = document.querySelector("#start-btn");
 
+var audioElement = document.createElement("audio");
+audioElement.setAttribute("src", "Beatrich.mp3");
 function displayScores() {
     if (storedScores !== null) {
         var scoreList = document.createElement("ol");
         scoreList.className = "scoreListClass";
         for (var i = 0; i < storedScores.length; i++) {
             var initials = storedScores[i].inits;
-            var scores = storedScores[i].userScore
+            var scores = storedScores[i].userScore;
             var scoreEntry = document.createElement("li");
             scoreEntry.innerHTML = initials + " - " + scores;
             scoreList.appendChild(scoreEntry);
@@ -22,6 +24,8 @@ displayScores();
 again.addEventListener("click", function () {
     location.href = "index.html";
 });
+
+// .sort(function(a, b){return a-b});
 
 clearBtn.addEventListener("click", function () {
     highScores.innerHTML = "";
